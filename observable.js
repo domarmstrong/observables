c645ac1c984e4157001c5e58b5b6ad428de75c62
Dom Armstrong <dom@dom-armstrong.co.uk>
2013-12-01 17:55:44 +0000

    Updated observables to use inheritance
    Since they use inheritance and are now objects use get and set.
    Updated tests.

--- observable.js
+++ observable.js
@@ -1,88 +1,183 @@
+"use strict";
+
 module.exports = (function () {
+    var tracking;
     var _ = {
-        'observables': []
+        'observables': [],
+        'collectDependencies': function ( computed ) {
+            tracking = computed;
+        },
+        'stopDependencyCollection': function () {
+            tracking = null;
+        },
+        'called': function ( observable ) {
+            if (tracking.dependencies.indexOf( observable ) === -1) {
+                tracking.dependencies.push( observable );
+                observable.subscribe( tracking.run.bind( tracking ) );
+            }
+        }
     };
 
-    return {
-        /**
-        * var o = new ob.Observable( initialValue );
-        * returns an observable function.
-        *
-        * A subscriber will be called any time an observables value changes
-        * o.subscribe(function ( value ) { console.log( value ) });
-        *
-        * Use o.notifyChange() to force all subscribers to be run with the current value;
-        */
-        'Observable': function ( initValue ) {
-            var ob = function () { return update.apply( ob, arguments ) };
-
-            ob.value = initValue;
-            ob._subscribers = [];
-            ob._namedSubscribers = [];
-            ob.subscribe = subscribe.bind( ob );
-            ob.unsubscribe = unsubscribe.bind( ob );
-            ob.notifyChange = notifyChange.bind( ob );
-            ob.equals = equals;
-
-
-            _.observables.push( ob );
-
-            return ob;
+    /**
+     * var s = new Subscribable();
+     *
+     * s.subscribe(function ( value ) { console.log( value ) });
+     * A subscriber fn will be called any time s.notifyChange() is called
+     */
+    function Subscribable() {
+        if (this === undefined) {
+            throw new Error('Subscribable is a constructor and should be called with "new"');
         }
+        this.init();
     }
-}());
+    Subscribable.prototype.init = function () {
+        this.subcriptions = [];
+        this._namedSubscribers = [];
+        this.value;
+    };
+    /**
+    * subscribe( function () {} );
+    * subscribe( 'namespace', function () {} );
+    */
+    Subscribable.prototype.subscribe = function subscribe() {
+        var ns, fn;
+        if (arguments.length === 1) {
+            fn = arguments[0];
+        } else {
+            ns = arguments[0];
+            fn = arguments[1];
+            // Only one function per namespace
+            this.unsubscribe( ns );
+            // This holds the index that the fn will be stored at
+            this._namedSubscribers[ this.subcriptions.length ] = ns;
+        }
+        if (typeof fn !== 'function') {
+            throw new Error( 'Subscribe only accepts functions' );
+        }
+        this.subcriptions.push( fn );
+    };
+    /**
+    * unsubscribe a function by pointer or by namespace
+    */
+    Subscribable.prototype.unsubscribe = function unsubscribe( namespace ) {
+        var index;
+        if (typeof namespace === 'string') {
+            index = this._namedSubscribers.indexOf( namespace );
+            this._namedSubscribers.splice( index, 1 );
+        } else {
+            index = this.subcriptions.indexOf( namespace );
+            if (this._namedSubscribers[ index ] != undefined) {
+                this._namedSubscribers.splice( index, 1 );
+            }
+        }
+        this.subcriptions.splice( index, 1 );
+    };
+    /**
+    * Call all subscribers with the latest value
+    */
+    Subscribable.prototype.notifyChange = function notifyChange() {
+        for (var i = 0; i < this.subcriptions.length; i++) {
+            this.subcriptions[ i ]( this.value );
+        };
+    };
+    Subscribable.prototype.dispose = function dispose() {
+        this.subcriptions = null;
+        this._namedSubscribers = null;
+        this.value = null;
+    };
 
-function update( v ) {
-    if (arguments.length > 0) {
+
+    /**
+    * var o = new Observable( initValue );
+    * returns an observable object that inherits from Subscribable
+    */
+    function Observable( initValue ) {
+        if (this === undefined) {
+            throw new Error('Observable is a constructor and should be called with "new"');
+        }
+        this.init( initValue );
+    }
+    // Inherit from Subscribable
+    Observable.prototype = Object.create(Subscribable.prototype);
+    Observable.prototype.init = function ( initValue ) {
+        Subscribable.prototype.init.call( this );
+        this.value = initValue;
+        _.observables.push( this );
+    };
+    Observable.prototype.equals = equals;
+    Observable.prototype.set = function ( v ) {
         if (this.equals( v, this.value )) return;
         this.value = v;
         this.notifyChange();
-    } else {
+        return this;
+    };
+    Observable.prototype.get = function () {
+        if (tracking) {
+            _.called( this );
+        }
         return this.value;
+    };
+
+    /**
+    * var c = new Computed( function () {} );
+    * returns a computed object that inherits from Subscribable
+    */
+    function Computed( fn ) {
+        if (this === undefined) {
+            throw new Error('Computed is a constructor and should be called with "new"');
+        }
+        if (typeof fn !== 'function') {
+            throw new Error('A computed requires a function as the first argument');
+        }
+        this.init( fn );
     }
-}
+    // Inherit from Subscribable
+    Computed.prototype = Object.create(Subscribable.prototype);
+    Computed.prototype.init = function ( fn ) {
+        Subscribable.prototype.init.call( this );
+        this.fn = fn;
+        this.dependencies = [];
+        _.collectDependencies( this );
+        this.value = fn();
+        _.stopDependencyCollection();
+
+        _.observables.push( this );
+    };
+    Computed.prototype.equals = equals;
+    Computed.prototype.run = function () {
+        var v = this.fn();
+        if (this.equals( v, this.value )) return;
+        this.value = v;
+        this.notifyChange();
+        return this;
+    };
+    Computed.prototype.get = function () {
+        if (tracking) {
+            _.called( this );
+        }
+        return this.fn();
+    };
+    Computed.prototype.dispose = function () {
+        Subscribable.prototype.dispose.call( this );
+
+        while (this.dependencies.length) {
+            var dep = this.dependencies.pop();
+            dep.unsubscribe( this.run );
+        }
+        this.dependencies = null;
+        this.fn = null;
+    };
+
+    return {
+        'Subscribable': Subscribable,
+        'Observable': Observable,
+        'Computed': Computed
+    }
+}());
+
 /**
  * The default equality comparer
  */
 function equals( v, nv ) {
     return v === nv;
-}
-/**
- * subscribe( function () {} );
- * subscribe( 'namespace', function () {} );
- */
-function subscribe() {
-    var ns, fn;
-    if (arguments.length === 1) {
-        fn = arguments[0];
-    } else {
-        ns = arguments[0];
-        fn = arguments[1];
-        // Only one function per namespace
-        this.unsubscribe( ns );
-        // This holds the index that the fn will be stored at
-        this._namedSubscribers[ this._subscribers.length ] = ns;
-    }
-    if (typeof fn !== 'function') {
-        throw new Error( 'Subscribe only accepts functions' );
-    }
-    this._subscribers.push( fn );
-};
-/**
- * unsubscribe a function by pointer or by namespace
- */
-function unsubscribe( namespace ) {
-    var index;
-    if (typeof namespace === 'string') {
-        index = this._namedSubscribers.indexOf( namespace );
-        this._namedSubscribers.splice( index, 1 );
-    } else {
-        index = this._subscribers.indexOf( namespace );
-    }
-    this._subscribers.splice( index, 1 );
-};
-function notifyChange() {
-    for (var i = 0; i < this._subscribers.length; i++) {
-        this._subscribers[ i ]( this.value );
-    };
 }
--- test/observable.js
+++ test/observable.js
@@ -1,45 +1,40 @@
 var assert = require('chai').assert;
 var ob = require('../observable');
 
-describe('Ob', function () {
-    it('Should be an object', function () {
-        assert.typeOf(ob, 'object');
-    });
-    it('Should have property obeservable', function () {
-        assert.property(ob, 'Observable');
-    });
-});
+var Subscribable = ob.Subscribable;
+var Observable = ob.Observable;
+var Computed = ob.Computed;
 
-describe('ob.Observable', function () {
+function testSubscribable() {
     var o;
-    it ('Should be a function', function () {
-        assert.typeOf(ob.Observable, 'function');
+    // The value of this is set when the function if called
+    var Constructor = this;
+    var initValue;
+    if (Constructor.name === 'Observable') {
+        initValue = 5;
+    } else if (Constructor.name === 'Computed') {
+        initValue = function () {};
+    }
+
+    it('Should be a function', function () {
+        assert.typeOf(Constructor, 'function');
     });
-    it('Should return a function', function () {
-        assert.typeOf(new ob.Observable(), 'function');
+    it('Should throw an error if not called with new', function () {
+        assert.throw(Constructor, Error, Constructor.name + ' is a constructor and should be called with "new"');
     });
-    it('Should accept a value and return its value if called with no arguments', function () {
-        o = new ob.Observable();
-        assert.equal(o(), undefined);
-        o('test');
-        assert.equal(o(), 'test');
-        o(undefined);
-        assert.equal(o(), undefined);
-    });
-    it('Can be instantiated with a value', function () {
-        o = new ob.Observable('test');
-        assert.equal(o(), 'test');
+    it('Should return an instance of Subscribable', function () {
+        assert.instanceOf(new Constructor(initValue), Subscribable);
     });
 
     before(function () {
-        o = new ob.Observable();
+        o = new Constructor(initValue);
     });
     it('Should have a property subscribe that is a function', function () {
         assert.typeOf(o.subscribe, 'function');
     });
 
-    describe('subscribe', function () {
-        o = new ob.Observable();
+    describe(Constructor.name + ' subscribe', function () {
+        o = new Constructor(initValue);
         it('Requires a function to be passed in', function () {
             o.subscribe(function () {});
             assert.throw(o.subscribe.bind(null, 'a'), Error, 'Subscribe only accepts functions');
@@ -52,52 +47,207 @@ describe('ob.Observable', function () {
             o.subscribe('sub', function () {});
         });
         it('Can be unsubscribed by name', function () {
-            o = new ob.Observable();
+            o = new Constructor(initValue);
             o.subscribe('sub', function () { assert.fail(); });
             o.unsubscribe('sub');
-            o(5);
+            o.notifyChange();
         });
-        it('Should only allow one subscriber per namespace', function () {
-            o = new ob.Observable();
+        it('Can be unsubscribed by pointer', function () {
+            o = new Constructor(initValue);
+            var fn = function () { assert.fail(); };
+            o.subscribe('sub', fn);
+            o.unsubscribe(fn);
+            o.notifyChange();
+            assert.lengthOf(o._namedSubscribers, 0);
+            assert.lengthOf(o.subcriptions, 0);
+
+            o = new Constructor(initValue);
+            var fn = function () { assert.fail(); };
+            o.subscribe(fn);
+            o.unsubscribe(fn);
+            o.notifyChange();
+            assert.lengthOf(o._namedSubscribers, 0);
+            assert.lengthOf(o.subcriptions, 0);
+        });
+        it('Should only allow one subscriber function per namespace', function () {
+            o = new Constructor(initValue);
             o.subscribe('sub', function () { assert.fail(); });
             o.subscribe('sub', function () {});
             o.notifyChange();
             assert.lengthOf(o._namedSubscribers, 1);
-            assert.lengthOf(o._subscribers, 1);
+            assert.lengthOf(o.subcriptions, 1);
         });
-        it('Should be stored in an array of _subscribers on its observable', function () {
-            function x() {};
-            o.subscribe(x);
-            assert(o._subscribers.indexOf(x) !== -1, 'subscriber not found in _subscribers');
+        it('Should be stored in an array of subcriptions', function () {
+            function fn() {};
+            o.subscribe(fn);
+            assert(o.subcriptions.indexOf(fn) !== -1, 'subscriber not found in subcriptions');
         });
+    });
+}
+
+describe('Subscribable', testSubscribable.bind(Subscribable));
+
+describe('Observable', function () {
+    it('Should inherit from Subscribable and should pass all Subscribable tests', function () {
+        testSubscribable.call(Observable); 
+    });
+    it('Should return an instance of Observable', function () {
+        assert.instanceOf(new Observable(), Observable);
+    });
+    it('Should have a property subcriptions', function () {
+        var o = new Observable();
+        assert.property(o, 'subcriptions');
+        assert.isArray(o.subcriptions);
+    });
+    it('Should have a property _namedSubscribers', function () {
+        var o = new Observable();
+        assert.property(o, '_namedSubscribers');
+        assert.isArray(o._namedSubscribers);
+    });
+    it('Should accept a value and return its value if called with no arguments', function () {
+        o = new Observable();
+        assert.equal(o.get(), undefined);
+        o.set('test');
+        assert.equal(o.get(), 'test');
+        o.set(undefined);
+        assert.equal(o.get(), undefined);
+    });
+    it('Can be instantiated with a value', function () {
+        o = new Observable('test');
+        assert.equal(o.get(), 'test');
     });
     it('Should call its subscribers with its value when its value changes', function () {
         var val;
-        o = new ob.Observable();
+        o = new Observable();
         o.subscribe(function (v) {
             val = v;
         });
         assert.equal(val, undefined);
-        o(5);
+        o.set(5);
         assert.equal(val, 5);
     });
     it('Should not call its subscribers if its value does not change', function () {
         var count = 0;
-        o = new ob.Observable(4);
+        o = new Observable(4);
         o.subscribe(function (v) {
             count++;
         });
         assert.equal(count, 0);
-        o(4);
+        o.set(4);
         assert.equal(count, 0);
     });
     it('Should accept a custom equality comparison function', function () {
-        o = new ob.Observable([1,2,3]);
+        o = new Observable([1,2,3]);
         o.equals = function (a, b) {
             return a.length == b.length;
         };
-        o.subscribe(function () { assert.fail('Not equal'); });
-        o([5,6,7]);
+        o.subscribe(function () { assert(false, 'Should not run'); });
+        o.set([5,6,7]);
     });
 });
 
+describe('Computed', function () {
+    it('Should inherit from Subscribable and should pass all Subscribable tests', function () {
+        testSubscribable.call(Computed); 
+    });
+    it('Should be an instance of Computed', function () {
+        assert.instanceOf(new Computed( function () {} ), Computed);
+    });
+    it('Should have a property subcriptions', function () {
+        var c = new Computed( function () {} );
+        assert.property(c, 'subcriptions');
+        assert.isArray(c.subcriptions);
+    });
+    it('Should have a property _namedSubscribers', function () {
+        var c = new Computed( function () {} );
+        assert.property(c, '_namedSubscribers');
+        assert.isArray(c._namedSubscribers);
+    });
+    it('Should have a method "get" that returns the current value', function () {
+        var c = new Computed( function () { return 2; } );
+        assert.equal(c.get(), 2);
+    });
+    it('Should have a methon "run" that forces the computed to be evaluated', function () {
+        var hasRun;
+        var c = new Computed( function () { hasRun = true; } );
+        hasRun = false;
+        c.run();
+        assert(hasRun, 'The computed was not run when "run" was called');
+    });
+    it('Should create a list of any Observable or Computed objects that are read when the computed function is initialised', function () {
+        var o = new Observable();
+        var co = new Computed( function () {} );
+        var c = new Computed(function () {
+            o.get(); 
+            co.get();
+        });
+        assert(c.dependencies.indexOf(o) !== -1, 'Observable has not been noted as a dependency');
+        assert(c.dependencies.indexOf(co) !== -1, 'Computed has not been noted as a dependency');
+    });
+    it('Should not create duplicate dependencies', function () {
+        var o = new Observable();
+        var c = new Computed(function () {
+            o.get(); 
+            o.get();
+        });
+        assert.lengthOf(c.dependencies, 1);
+    });
+    it('Should be run if any of it dependencies values change', function () {
+        var hasRun;
+        var o = new Observable();
+        var c = new Computed(function () {
+            o.get(); 
+            hasRun = true;
+        });
+        hasRun = false;
+        o.notifyChange();
+        assert(hasRun, 'The computed was not run when its dependency changed');
+    });
+    it('Should call its subscribers with its value when its value changes', function () {
+        var value;
+        o = new Observable(5);
+        c = new Computed(function () { return o.get(); });
+        c.subscribe(function (v) {
+            value = v;
+        });
+        o.set(6);
+        assert.equal(value, 6, 'Subscription was not called with the latest value when computed updated');
+    });
+    it('Should not call its subscribers if its value does not change', function () {
+        var hasRun = false;
+        o = new Observable(5);
+        c = new Computed(function () { return o.get(); });
+        c.subscribe(function (v) {
+            hasRun = true;
+        });
+        o.notifyChange();
+        assert.notOk(hasRun, 'Subscription was called when computed updated');
+    });
+    it('Should accept a custom equality comparison function', function () {
+        o = new Observable([1,2,3]);
+        c = new Computed(function () { return o.get(); });
+        c.equals = function (a, b) {
+            return a.length == b.length;
+        };
+        c.subscribe(function (v) {
+            assert(false, 'Should not run');
+        });
+        o.set([5,6,7]);
+    });
+    it('Should have a method "dispose" that cleans itself up', function () {
+        c = new Computed(function () { return o.get(); });
+        c.dispose();
+        Object.keys(c).forEach(function (key) {
+            if (c[key] !== null) assert(false, '"' + key + '" is not null')
+        });
+    });
+    it('Should unlink itself from its dependencies if it is disposed', function () {
+        o = new Observable(1);
+        c = new Computed(function () { return o.get(); });
+        c.subscribe(function (v) {
+            assert(false, 'Should not run');
+        });
+        c.dispose();
+        o.set(2);
+    });
+});
